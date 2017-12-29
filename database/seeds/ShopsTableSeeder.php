206d1b92640bb8a7a4983a380bdc1e92519d2f0a
Hamza Bentahar <59384@aui.ma>
2017-12-29 12:42:56 +0000

    seed

---
+++ database/seeds/ShopsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ShopsTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $shops = json_decode(\Illuminate\Support\Facades\File::get('database/seeds/shops.json'));
+    foreach ($shops as $shop) {
+      $shopsObj = new \App\Shop();
+      $shopsObj->picture = $shop->picture;
+      $shopsObj->name = $shop->name;
+      $shopsObj->email = $shop->email;
+      $shopsObj->city = $shop->city;
+      $shopsObj->latitude = $shop->location->coordinates[0];
+      $shopsObj->longitude = $shop->location->coordinates[1];
+      $shopsObj->save();
+    }
+  }
+}
---
+++ resources/assets/js/components/Shops.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "shops"
+  }
+</script>
+
+<style scoped>
+
+</style>
